59089015cd7685546b1ccc4fa2d5ef13fbb5cf3e
dan <wdanxna@gmail.com>
2024-10-06 18:34:09 +1300

    add, cmp, sub, jump

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ My homework assignment for performance awareness programming
 06/10/2024
 Complete assignment 1, 2 and challenge assignment:
 An 8086 disassembler, can decode all MOV instructions now
+
+assignment 3: implement ADD, SUB, CMP, JNZ decoding
--- disasm.cpp
+++ disasm.cpp
@@ -103,18 +103,22 @@ std::string next_immediate(uint8_t w, std::ifstream &s, bool prefix = true) {
   }
 }
 
-std::string register_memory_to_from_register(char op, std::ifstream &s) {
-  // at least 2 bytes instruction
-  char b2{};
-  if (!s.read(&b2, sizeof(char))) {
+std::tuple<uint8_t, uint8_t, uint8_t>
+next_mod_reg_rm(std::ifstream &s) {
+  char b{};
+  if (!s.read(&b, sizeof(char))) {
     assert(false);
   }
-  uint8_t dw = (op & 0b00000011);
-  uint8_t mod = (b2 & 0b11000000) >> 6;
-  uint8_t reg = (b2 & 0b000111000) >> 3;
-  uint8_t rm = (b2 & 0b000000111);
+  uint8_t mod = (b & 0b11000000) >> 6;
+  uint8_t reg = (b & 0b000111000) >> 3;
+  uint8_t rm = (b & 0b000000111);
+  return {mod, reg, rm};
+}
 
-  std::ostringstream oss;
+std::tuple<std::string, std::string>
+next_reg_mem(char op, std::ifstream &s) {
+  auto [mod, reg, rm] = next_mod_reg_rm(s);
+  uint8_t dw = (op & 0b00000011);
   std::string src, dest;
   src = get_register(reg, dw & 1);
   switch (mod) {
@@ -127,13 +131,16 @@ std::string register_memory_to_from_register(char op, std::ifstream &s) {
     dest = get_effective_address(rm, mod, s);
     break;
   }
-
   if (dw & 0b10) {
     // d is 1, use reg as destination
     std::swap(src, dest);
   }
-  oss << "mov " << dest << "," << src;
-  return oss.str();
+  return {src, dest};
+}
+
+std::string register_memory_to_from_register(char op, std::ifstream &s) {
+  auto [src, dest] = next_reg_mem(op, s);
+  return (std::stringstream() << "mov " << dest << "," << src).str();
 }
 
 std::string immediate_to_memory(char op, std::ifstream &s) {
@@ -167,8 +174,143 @@ std::string accumulator_to_memory(char op, std::ifstream &s) {
   return "mov [" + next_immediate(op & 1, s, false) + "], ax";
 }
 
-std::string decode(char op, std::ifstream &s) {
+using SS = std::stringstream;
+std::string add_cmp_sub_rm_to_rm(char op, std::ifstream &s) {
+    std::string inst;
+    switch ((op & 0b111000)>>3)
+    {
+    //ADD
+    case 0b000:
+        inst = "add";
+        break;
+    case 0b111:
+        inst = "cmp";
+        break;
+    //SUB
+    case 0b101:
+        inst = "sub";
+    default:
+        break;
+    }
+
+    auto [src, dest] = next_reg_mem(op, s);
+    return (SS() << inst << " " << dest << ", " << src).str();
+}
+
+std::string add_cmp_sub_imm_to_acc(char op, std::ifstream &s) {
+    std::string inst;
+    switch ((op & 0b111000)>>3)
+    {
+    //ADD
+    case 0b000:
+        inst = "add";
+        break;
+    //CMP
+    case 0b111:
+        inst = "cmp";
+        break;
+    //SUB
+    case 0b101:
+        inst = "sub";
+    default:
+        break;
+    }
+    auto imm = next_immediate(op&1, s);
+    return (SS() << inst << " ax, " << imm).str();
+}
+
+std::string add_cmp_sub_imm_to_rm(char op, std::ifstream &s) {
+    auto [mod, reg, rm] = next_mod_reg_rm(s);
+    std::string inst;
+    switch (reg)
+    {
+    //ADD
+    case 0b000:
+        inst = "add";
+        break;
+    //CMP
+    case 0b111:
+        inst = "cmp";
+        break;
+    //SUB
+    case 0b101:
+        inst = "sub";
+        break;
+    default:
+        break;
+    }
+
+    std::string dest;
+    if (mod == 0b11) {
+        dest = get_register(reg, op&1);
+    }
+    else {
+        dest = get_effective_address(rm, mod, s);
+    }
+
+    uint8_t signext = (op&0b10) >> 1;
+    uint8_t w = (op&1);
+    std::string src = next_immediate((!signext)&w, s);
+
+    return (SS() << inst << " " << dest << ", " << src).str();
+}
+
+int8_t next_INC8(std::ifstream& s) {
+    //next 8-bit signed increment (IP-INC8)
+    int8_t inc{};
+    if (!s.read((char *)&inc, sizeof(int8_t))) {
+      assert(false);
+    }
+    return inc;
+}
+
+//conditional jump
+std::string jxx(const std::string& instruct, std::ifstream &s) {
+    return (SS() << instruct << " " << std::to_string(next_INC8(s))).str();
+}
+
+std::string decode(uint8_t op, std::ifstream &s) {
   switch (op) {
+  case 0b01110101:
+    return jxx("jnz", s);
+  case 0b01110100:
+    return jxx("je", s);
+  case 0b01111100:
+    return jxx("jl", s);
+  case 0b01111110:
+    return jxx("jle", s);
+  case 0b01110010:
+    return jxx("jb", s);
+  case 0b01110110:
+    return jxx("jbe", s);
+  case 0b01111010:
+    return jxx("jp", s);
+  case 0b01110000:
+    return jxx("jo", s);
+  case 0b01111000:
+    return jxx("js", s);
+  case 0b01111101:
+    return jxx("jnl", s);
+  case 0b01111111:
+    return jxx("jg", s);
+  case 0b01110011:
+    return jxx("jnb", s);
+  case 0b01110111:
+    return jxx("ja", s);
+  case 0b01111011:
+    return jxx("jnp", s);
+  case 0b01110001:
+    return jxx("jno", s);
+  case 0b01111001:
+    return jxx("jns", s);
+  case 0b11100010:
+    return jxx("loop", s);
+  case 0b11100001:
+    return jxx("loopz", s);
+  case 0b11100000:
+    return jxx("loopnz", s);
+  case 0b11100011:
+    return jxx("jcxz", s);
   case 0b10001100:
     // segment register to register/memory
     break;
@@ -188,10 +330,24 @@ std::string decode(char op, std::ifstream &s) {
     case 0b10100010:
       // accumulator to memory
       return accumulator_to_memory(op, s);
+    //ADD & CMP & SUB imm to accumulator
+    case 0b00000100:
+    case 0b00111100:
+    case 0b00101100:
+        return add_cmp_sub_imm_to_acc(op, s);
     default:
       switch (op & 0b11111100) {
+      //ADD & CMP & SUB rm to rm
+      case 0b00000000:
+      case 0b00111000:
+      case 0b00101000:
+        return add_cmp_sub_rm_to_rm(op, s);
+      //ADD & CMP & SUB imm to rm
+      case 0b10000000:
+        return add_cmp_sub_imm_to_rm(op, s);
+
+      // register/memory to/from register
       case 0b10001000:
-        // register/memory to/from register
         return register_memory_to_from_register(op, s);
       default:
         switch (op & 0b11110000) {
@@ -211,8 +367,8 @@ std::string decode(char op, std::ifstream &s) {
 }
 
 std::string decode(std::ifstream &s) {
-  char op{};
-  if (!s.read(&op, sizeof(char)))
+  uint8_t op{};
+  if (!s.read((char*)&op, sizeof(char)))
     return "";
   return decode(op, s);
 }
